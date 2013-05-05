cbe41488119d552241b2a4140913914a77bc16de
Kourse12 <Kourse12@c166c940-e59a-fa92-b3a0-cba52cc8559b>
2013-05-05 20:58:13 +0000

    Lab 3 completed. All tests pass. Modules Included: RegisterFile, DataMemory

---
+++ mips_tamu/tags/lab3/hw/DataMemory.v
@@ -0,0 +1,32 @@
+`ifndef _datamemory_v_
+`define _datamemory_v_
+`endif
+
+
+
+module DataMemory ( ReadData , Address , WriteData , MemoryRead , MemoryWrite , Clock ) ;
+
+  parameter DataMemory_Size = 256;              // in bytes
+
+  output reg [31:0] ReadData;
+  input wire [5:0] Address; 
+  input wire [31:0] WriteData;
+  input wire MemoryRead, MemoryWrite, Clock;
+  
+  reg [31:0] data [DataMemory_Size/4:0];        // The actual stored data
+  
+  always @(*) begin                             
+    if (MemoryRead)
+      ReadData <= data[Address];                
+    else
+      ReadData <= ReadData;                     // If read enable is low, retain previous value
+  
+  end
+  
+  always @(negedge Clock) begin                  // Writes synchronous with negedge Clock
+    if (MemoryWrite)
+      data[Address] <= WriteData;  
+  end  
+    
+
+endmodule
